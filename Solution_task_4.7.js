ae7889e75de435eacd776d65e7f9be57cfdf8e5a
Larin Artem <artem_larin@yoniti.ru>
2022-11-23 21:41:54 +0300

    add task 4.7

---
+++ Solution_task_4.7.js
@@ -0,0 +1,11 @@
+switch (range) {
+    case "day":
+      console.log(`${price} Р в день`);
+      break;
+    case "week":
+      console.log(`${price} Р в неделю`);
+      break;
+    case "month":
+      console.log(`${price} Р в месяц`);
+      break;
+  }
